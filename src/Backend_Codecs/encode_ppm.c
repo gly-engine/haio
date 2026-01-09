7250ec1dc38e6e1a92ec1a836d9b3738e87bac80
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2026-01-09 15:46:54 -0300

    feat: BufferPush43 & BufferPushString

--- src/Backend_Codecs/encode_ppm.c
+++ src/Backend_Codecs/encode_ppm.c
@@ -1,22 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 
 #include "haio.h"
 #include "haio/functions.h"
 
 /**
- * @pre needs @c handle.width and @c handle.height
  * @param [in,out] handle
  * @param [in] src
  * @param [out] dst
  */
 void EncodeRGBA8ToPPM(haio_worker_handle_t *handle, haio_buffer_t *src, haio_buffer_t *dst)
 {
-    // @todo move??
-    dst->len = 0;
-
     do {
         if (handle->state == HAIO_FSM_WORKER_DONE) {
             break;
@@ -32,36 +25,17 @@ void EncodeRGBA8ToPPM(haio_worker_handle_t *handle, haio_buffer_t *src, haio_buf
             if (handle->canvas.parent->width == 0 || handle->canvas.parent->height == 0) {
                 assert(false);
             }
-
-            BufferEnsureCapacity(dst, 32);
-
             handle->canvas.width = handle->canvas.parent->width;
             handle->canvas.height = handle->canvas.parent->height;
             handle->progress.nbytes.count = 0;
             handle->progress.nbytes.total = (handle->canvas.width * handle->canvas.height) * 3lu;
-            dst->len += (size_t) snprintf(dst->data.str, dst->size, "P6\n%d %d\n255\n", handle->canvas.width, handle->canvas.height);
+            BufferPushString(dst, "P6\n%d %d\n255\n", handle->canvas.width, handle->canvas.height);
             handle->state = HAIO_FSM_WORKER_RUNNING;
         }
 
-        if (src->len % 4 != 0) {
+        handle->progress.nbytes.count += ((src->len / 4) * 3);
+        if(BufferPush43(dst, src->data.ptr, src->len)) {
             assert(false);
-        }
-
-        size_t nbytes_rgb = ((src->len / 4) * 3);
-        BufferEnsureCapacity(dst, dst->len + nbytes_rgb);
-        uint8_t *dst_ptr = &dst->data.u8[dst->len];
-        uint8_t *dst_end = dst_ptr + nbytes_rgb;
-        uint8_t *src_ptr = src->data.u8;
-
-        handle->progress.nbytes.count += nbytes_rgb;
-        dst->len += nbytes_rgb;
-
-        // @todo otimze with SSSE3
-        while (dst_ptr < dst_end) {
-            *dst_ptr++ = *src_ptr++;
-            *dst_ptr++ = *src_ptr++;
-            *dst_ptr++ = *src_ptr++;
-            src_ptr++;
         }
 
         if (handle->progress.nbytes.count >= handle->progress.nbytes.total) {
--- src/Driver_Memory/buffer.c
+++ src/Driver_Memory/buffer.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 #include "haio.h"
 
 #define INITIAL_SIZE 512
 
-int BufferReset(haio_buffer_t *buf) {
-    buf->len = 0;
-    return 0;   
+void BufferReset(haio_buffer_t *buf) {
+    buf->len = 0;  
+    buf->pos = 0; 
 }
 
 int BufferEnsureCapacity(haio_buffer_t *buf, size_t len) {
@@ -35,10 +37,78 @@ int BufferEnsureCapacity(haio_buffer_t *buf, size_t len) {
 
 int BufferPush(haio_buffer_t *buf, const void *data, size_t len) {
     if (!buf) return 1;
-    if (BufferEnsureCapacity(buf, len)) return 2;
+    if (BufferEnsureCapacity(buf, len + buf->len)) return 2;
 
     memcpy(buf->data.u8 + buf->len, data, len);
     buf->len += len;
+    return 0;
+}
+
+/**
+ * @brief Appends packed `4 -> 3` data to the buffer.
+ *
+ * @details Converts interleaved 4-byte elements into packed 3-byte elements
+ * by skipping the 4th byte (alpha channel). Commonly used to copy
+ * data from @c RGBA to @c RGB.
+ *
+ * @param[out] buf
+ * @param[in] data
+ * @param[in] len
+ *
+ * @retval 0 Success.
+ */
+int BufferPush43(haio_buffer_t *buf, const void *data, size_t len) {
+    if (!buf || !data) return 1;
+
+    size_t pixels = len / 4;
+    size_t out_len = pixels * 3;
+
+    if (pixels == 0) return 3;
+
+    size_t needed  = buf->len + out_len;
+    size_t rounded = (needed + 11) / 12 * 12;
+
+    if(BufferEnsureCapacity(buf, rounded)) return 2;
+
+    uint8_t *dst_ptr = &buf->data.u8[buf->len];
+    uint8_t *dst_end = &dst_ptr[out_len];
+    const uint8_t *src_ptr = (const uint8_t *)data;
+
+    while (dst_ptr < dst_end) {
+        *dst_ptr++ = *src_ptr++;
+        *dst_ptr++ = *src_ptr++;
+        *dst_ptr++ = *src_ptr++;
+        src_ptr++; 
+    }
+
+    buf->len += out_len;
+    return 0;
+}
+
+int BufferPushString(haio_buffer_t *buf, const char *fmt, ...)
+{
+    if (!buf || !fmt) return 1;
+
+    va_list args;
+    va_start(args, fmt);
+
+    va_list args_copy;
+    va_copy(args_copy, args);
+
+    int needed = vsnprintf(NULL, 0, fmt, args);
+    va_end(args);
+
+    if (needed < 0) {
+        va_end(args_copy);
+        return 2;
+    }
+
+    if(BufferEnsureCapacity(buf, buf->len + (size_t)needed + 1)) return 2;
+
+    vsnprintf(&buf->data.str[buf->len], buf->size, fmt, args_copy);
+    va_end(args_copy);
+
+    buf->len += (size_t)needed;
     return 0;
 }
 
@@ -50,15 +120,19 @@ size_t BufferPop(haio_buffer_t *buf, void *out, size_t len) {
 
     size_t to_copy = (len < available) ? len : available;
 
-
     memcpy(out, buf->data.u8 + buf->pos, to_copy);
     buf->pos += to_copy;
+
+    if (buf->pos >= buf->len) {
+        buf->len = 0;
+        buf->pos = 0;
+    }
 
     return to_copy;
 }
 
 bool BufferHasQeue(haio_buffer_t *buf) {
-    return buf->len > 0 && buf->pos > 0 && buf->pos < buf->len;
+    return buf && buf->len > 0 && buf->pos > 0 && buf->pos < buf->len;
 }
 
 /**
